713d659ff2bd7e9ccea4fdc2b4a13d7384f388ef
PMagombedze <percymagoras@outlook.com>
2024-02-21 18:55:27 +0200

    asyncio_

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,8 +10,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> float | int:
+async def wait_random(max_delay: int = 10) -> float:
     """wait random"""
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Defines concurrent execution function
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Execute multiple coroutines
+    """
+    sync = await asyncio.gather(
+        *(task_wait_random(max_delay) for i in range(n)))
+    return sorted(sync)
