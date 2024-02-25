da4f9c769d0a8cfb6737418378a220792c0360a1
PMagombedze <percymagoras@outlook.com>
2024-02-25 21:11:24 +0200

    safely get value

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -9,7 +9,7 @@ function safe_first_element
 from typing import Union, Sequence, Any
 
 
-# The types of the elements of the input are not know
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """annotate"""
     if lst:
